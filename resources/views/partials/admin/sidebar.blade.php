8d638a04809aaa048280eb276628f8ef8696ca0d
webdeveloper510 <developerweb6@gmail.com>
2024-08-06 13:09:27 +0000

    add: power BI

--- resources/views/partials/admin/sidebar.blade.php
+++ resources/views/partials/admin/sidebar.blade.php
@@ -280,6 +280,7 @@ $powerBiReports = PowerBiReport::all();
         </div>
     </div>
 </div>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 <script>
     function showAccordion(dataId) {
         $('.accordion-collapse').css('display', 'none')
